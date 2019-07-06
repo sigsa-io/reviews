fc7371356ee8eacf944eeff89a30eaa58778cf7a
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-05 22:11:03 -0700

    chore: Create webpack configuration file
    Extends functionality to transpiling .js and .jsx files using Babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+
+  resolve: { extensions: ['.js', '.jsx'] },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env'],
+        },
+      },
+    ],
+  },
+};
