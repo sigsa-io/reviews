646ca9695f6348ad2c9c084e0a2eeb5ecb892c6a
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-08 22:05:29 -0700

    feat: Outline summary content

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
-const Summary = () => (
+const Summary = () => {
+
+  let averageRating = 4.9;
+  let reviewersCount = 55;
+
+  return(
+    <div id="reviews-summary" className="reviews-summary">
   <div>
     <h2>Summary</h2>
   </div>
+      <div className="reviews-summary-wrapper">
+        <div className="reviews-summary-title">
+          What {reviewersCount} People Are Saying
+        </div>
+        <div className="reviews-summary-overall-wrapper">
+          <div>
+            <div className="reviews-summary-subtitle">
+              Overall ratings and reviews
+            </div>
+            <div className="reviews-summary-overall-text">
+              Reviews can only be made by diners who have eaten at this restaurant
+            </div>
+            <div className="reviews-summary-overall-rating-average-wrapper">
+              <div className="reviews-star-rating-wrapper">
+                ★★★★★
+              </div>
+              <div className="reviews-short-text">
+                <span>{averageRating}</span>
+                <span>based on recent ratings</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 );
+};
 
 export default Summary;
