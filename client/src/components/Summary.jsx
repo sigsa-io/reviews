68b42646df285924614022703f1b45f29c28c546
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 12:35:05 -0700

    refactor: Create separate subcomponent for Awards

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Awards from './Awards';
 
 const Summary = ({
   summary: {
@@ -15,8 +16,7 @@ const Summary = ({
   const ratingCategories = [5, 4, 3, 2, 1];
 
   const awards = ['Best Overall', 'Best Food', 'Best Service', 'Best Ambiance', 'Fit for Foodies', 'Notable Wine List', 'Special Occasion', 'American', 'Best Overall', 'Best Food', 'Best Service', 'Best Ambiance', 'Fit for Foodies', 'Healthy', 'Notable Wine List', 'Special Occasion', 'American'];
-  const restaurant_locality = 'San Francisco Bay Area';
-  const numberOfDisplayedAwards = 3;
+  const awardLocality = 'San Francisco Bay Area';
 
   return (
     <div id="reviews-summary" className="reviews-summary">
@@ -81,45 +81,7 @@ const Summary = ({
         </div>
 
 
-        <div>
-          <div>
-            <div className="reviews-summary-subtitle">Loved For</div>
-            <div className="reviews-summary-awards-wrapper">
-              <input id="dinersChoiceShowMore" type="checkbox" style={{ display: 'none' }} />
-
-              {awards.map((award, key) => (
-                <object key={key} data-hide={key > numberOfDisplayedAwards - 1} className="reviews-summary-awards-individual-wrapper">
-                  <div className="reviews-summary-awards-individual-container">
-                    <div className="reviews-summary-awards-individual">
-                      <div className="reviews-summary-awards-individual-icon-wrapper reviews-individual-icon-wrapper">
-                        <i className="reviews-summary-awards-individual-icon" />
-                      </div>
-                      <div className="reviews-individual-text-wrapper">
-                        <div className="reviews-summary-awards-individual-name">{award}</div>
-                        <div className="reviews-summary-awards-individual-locality">{restaurant_locality}</div>
-                      </div>
-                    </div>
-                  </div>
-                </object>
-              ))}
-              {awards.length <= 3 ? <div /> : (
-                <label htmlFor="dinersChoiceShowMore" className="reviews-summary-awards-individual-wrapper">
-                  <div className="reviews-summary-awards-individual-container">
-                    <div className="reviews-summary-awards-individual">
-                      <div className="reviews-individual-text-wrapper">
-                        <div className="reviews-summary-awards-individual-name">
-                          {`+${awards.length - numberOfDisplayedAwards} More`}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </label>
-              )}
-
-            </div>
-          </div>
-        </div>
-
+            <Awards awards={awards} awardLocality={awardLocality}/>
 
       </div>
     </div>
