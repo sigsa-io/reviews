39dd0ce2a5257683a284d1dab06c0de8cdbc33b2
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 15:23:27 -0700

    feat: Update review List subcomponent

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Review from './Review';
 
-const List = () => (
-  <div>
-    <h2>Reviews List</h2>
-    <Review />
+const List = ({ reviews }) => (
+  <div id="reviews-list" className="reviewsList">
+    {reviews.map( (review, key) =>
+      <div key={key}>
+        <Review review={review} />
+      </div>
+    )}
   </div>
 );
+
+List.propTypes = {
+  reviews: PropTypes.array.isRequired,
+};
 
 export default List;
