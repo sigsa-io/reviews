99a171b7310245e3fc460960d138493c26d0fab0
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 15:22:42 -0700

    feat: Create Toolbar subcomponent

---
+++ client/src/components/Toolbar.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Toolbar = () => (
+    <div id="reviews-toolbar" className="reviews-toolbar">
+        {/* Toolbar (Sort and filter) */}
+    </div>
+);
+
+Toolbar.propTypes = {
+};
+
+export default Toolbar;
