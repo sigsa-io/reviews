92041a1a4cb70e1b9afb2d9c757d539b5db220da
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 15:39:06 -0700

    Refactor: Update for multiline func body

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Review = ({review}) => (
+const Review = ({
+  review,
+}) => {
+  return (
   <div>
     <h3>Review {review.review_id}</h3>
   </div>
 );
+};
 
 Review.propTypes = {
   review: PropTypes.object.isRequired,
