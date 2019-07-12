b6ab9c23a9365d6cc9744b8dea5ea19750f1bb66
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 15:24:47 -0700

    feat: Update Review subcomponent

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,9 +1,14 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const Review = () => (
+const Review = ({review}) => (
   <div>
-    <h3>Review #1</h3>
+    <h3>Review {review.review_id}</h3>
   </div>
 );
+
+Review.propTypes = {
+  review: PropTypes.object.isRequired,
+};
 
 export default Review;
