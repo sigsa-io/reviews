8c6b28aeffc81ed00e4a32308505dcc05bf141fd
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 16:34:50 -0700

    refactor: Separate Review into subcomponents

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,44 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Author from './Author';
+import Content from './Content';
 
 const Review = ({
   review,
   review: {
     review_id,
-    user_initials,
-    user_name,
-    user_location,
-    user_review_count,
-    dine_date,
-    rating_overall,
-    rating_food,
-    rating_service,
-    rating_ambience,
-    helpful_count,
-    body,
   },
 }) => {
+
   return (
-  <div>
-    <h3>Review {review.review_id}</h3>
-  </div>
-);
+    <div id={review.review_id} className="reviewListItem reviews-individual-review-wrapper">
+      <div
+        id={`review-${review.review_id}`}
+        className="reviews-individual-review"
+      >
+        <Author review={review} />
+        <Content review={review} />
+      </div>
+    </div>
+   );
 };
 
 Review.propTypes = {
   review: PropTypes.object.isRequired,
   review_id: PropTypes.number,
-  user_initials: PropTypes.string,
-  user_name: PropTypes.string,
-  user_location: PropTypes.string,
-  user_review_count: PropTypes.number,
-  dine_date: PropTypes.string,
-  rating_overall: PropTypes.number,
-  rating_food: PropTypes.number,
-  rating_service: PropTypes.number,
-  rating_ambience: PropTypes.number,
-  helpful_count: PropTypes.number,
-  body: PropTypes.string,
 };
 
 export default Review;
