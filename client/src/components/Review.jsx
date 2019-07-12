3dab04f20c8b3b7eaa3de35eb721714e1bb574af
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-12 15:43:39 -0700

    feat: Define Review component props

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 
 const Review = ({
   review,
+  review: {
+    review_id,
+    user_initials,
+    user_name,
+    user_location,
+    user_review_count,
+    dine_date,
+    rating_overall,
+    rating_food,
+    rating_service,
+    rating_ambience,
+    helpful_count,
+    body,
+  },
 }) => {
   return (
   <div>
@@ -13,6 +27,18 @@ const Review = ({
 
 Review.propTypes = {
   review: PropTypes.object.isRequired,
+  review_id: PropTypes.number,
+  user_initials: PropTypes.string,
+  user_name: PropTypes.string,
+  user_location: PropTypes.string,
+  user_review_count: PropTypes.number,
+  dine_date: PropTypes.string,
+  rating_overall: PropTypes.number,
+  rating_food: PropTypes.number,
+  rating_service: PropTypes.number,
+  rating_ambience: PropTypes.number,
+  helpful_count: PropTypes.number,
+  body: PropTypes.string,
 };
 
 export default Review;
