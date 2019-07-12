435d524790ba9f46af8593f04a0995d9b5cea09a
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-11 18:23:08 -0700

    feat: Pass initial State to App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import initialState from './initialState';
 
-ReactDOM.render(<App />,
+ReactDOM.render(<App initialState={initialState} />,
   document.getElementById('app'));
