fbe95b5482dfa22cfe145cc907dc451d9235bece
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-11 19:52:03 -0700

    feat: Display restaurant awards

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -14,7 +14,12 @@ const Summary = ({
 
   const ratingCategories = [5, 4, 3, 2, 1];
 
+  const awards = ['Best Overall', 'Best Food', 'Best Service', 'Best Ambiance', 'Fit for Foodies', 'Notable Wine List', 'Special Occasion', 'American', 'Best Overall', 'Best Food', 'Best Service', 'Best Ambiance', 'Fit for Foodies', 'Healthy', 'Notable Wine List', 'Special Occasion', 'American'];
+  const restaurant_locality = 'San Francisco Bay Area';
+  const numberOfDisplayedAwards = 3;
 
+  return (
+    <div id="reviews-summary" className="reviews-summary">
 
       <div className="reviews-summary-wrapper">
         <div className="reviews-summary-title">
@@ -51,7 +56,6 @@ const Summary = ({
               ))}
             </div>
           </div>
-          </div>
 
           <div className="reviews-summary-filter-overall-ratings-wrapper">
             <div className="reviews-summary-filter-overall-ratings">
@@ -69,11 +73,54 @@ const Summary = ({
                     <span />
                   </div>
                 </div>
+              ))}
+
+            </div>
+          </div>
+
+        </div>
+
+
+        <div>
+          <div>
+            <div className="reviews-summary-subtitle">Loved For</div>
+            <div className="reviews-summary-awards-wrapper">
+              <input id="dinersChoiceShowMore" type="checkbox" style={{ display: 'none' }} />
+
+              {awards.map((award, key) => (
+                <object key={key} data-hide={key > numberOfDisplayedAwards - 1} className="reviews-summary-awards-individual-wrapper">
+                  <div className="reviews-summary-awards-individual-container">
+                    <div className="reviews-summary-awards-individual">
+                      <div className="reviews-summary-awards-individual-icon-wrapper reviews-individual-icon-wrapper">
+                        <i className="reviews-summary-awards-individual-icon" />
+                      </div>
+                      <div className="reviews-individual-text-wrapper">
+                        <div className="reviews-summary-awards-individual-name">{award}</div>
+                        <div className="reviews-summary-awards-individual-locality">{restaurant_locality}</div>
+                      </div>
+                    </div>
+                  </div>
+                </object>
+              ))}
+              {awards.length <= 3 ? <div /> : (
+                <label htmlFor="dinersChoiceShowMore" className="reviews-summary-awards-individual-wrapper">
+                  <div className="reviews-summary-awards-individual-container">
+                    <div className="reviews-summary-awards-individual">
+                      <div className="reviews-individual-text-wrapper">
+                        <div className="reviews-summary-awards-individual-name">
+                          {`+${awards.length - numberOfDisplayedAwards} More`}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </label>
               )}
 
             </div>
           </div>
         </div>
+
+
       </div>
     </div>
 );
