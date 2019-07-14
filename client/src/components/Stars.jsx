e9bd893b5956adcd360a06c71b02370459ce6e2b
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-14 15:11:04 -0700

    feat: Creat Stars subcomponent to visualize rating

---
+++ client/src/components/Stars.jsx
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from '../styles/style.css'
+
+const Stars = ({
+  rating,
+}) => {
+  let fullStarsCount = Math.floor(rating);
+  let fractionalStar = Math.ceil((rating-fullStarsCount)*4)/4;
+  let emptyStarsCount = 5-fullStarsCount-Math.ceil(fractionalStar);
+
+  return (
+    <div className={styles[`reviews-star-rating-wrapper`]}>
+      {Array(fullStarsCount).fill(1).map((_unused,key)=>{
+        return(
+        <object key={key} className={styles[`reviews-star-rating-individual-wrapper`]}>
+          <div className={styles[`reviews-star-rating-individual-icon-full`]} />
+        </object>
+        )
+      })}
+
+      {
+        fractionalStar===0?
+          ``
+          :
+          fractionalStar===0.25?
+            <object className={styles[`reviews-star-rating-individual-wrapper`]}>
+              <div className={styles[`reviews-star-rating-individual-icon-quarter`]} />
+            </object>
+            :
+            fractionalStar===0.5?
+              <object className={styles[`reviews-star-rating-individual-wrapper`]}>
+                <div className={styles[`reviews-star-rating-individual-icon-half`]} />
+              </object>
+              :
+              fractionalStar===0.75?
+                <object className={styles[`reviews-star-rating-individual-wrapper`]}>
+                  <div className={styles[`reviews-star-rating-individual-icon-threequarter`]} />
+                </object>
+              :
+              <object className={styles[`reviews-star-rating-individual-wrapper`]}>
+                <div className={styles[`reviews-star-rating-individual-icon-full`]} />
+              </object>
+      }
+
+      {Array(emptyStarsCount).fill(1).map((_unused,key)=>{
+        return(
+        <object key={key} className={styles[`reviews-star-rating-individual-wrapper`]}>
+          <div className={styles[`reviews-star-rating-individual-icon-empty`]} />
+        </object>
+        )
+      })}
+
+    </div>
+  );
+};
+
+Stars.propTypes = {
+  rating: PropTypes.number,
+};
+
+export default Stars;
