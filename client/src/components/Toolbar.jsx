226e934c1f3b3ff2c91435b14e13784a856b529c
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-07-15 11:11:17 -0700

    test: Comment out components in development

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Sort from './Sort';
-import Filter from './Filter';
+// import Sort from './Sort';
+// import Filter from './Filter';
 import styles from '../styles/style.css';
 
 const Toolbar = () => (
     <div id="reviews-toolbar" className={styles[`reviews-toolbar`]}>
-        <Sort />
-        <Filter />
+        {/* <Sort /> */}
+        {/* <Filter /> */}
     </div>
 );
 
